f4fdb5f2cab3d5a69412b3eaf46a29d34696e8b7
ryannorton <nort.ryan@gmail.com>
2015-01-17 06:02:11 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route("/search")
+def search(item):
+	result = {
+		'item' : 'Baseball'
+		'store' : 'Walmart',
+		'price' : 35.62
+	}
+	return json.dumps(result)
+
+if __name__ == "__main__":
+    app.run()
+
+
+
+
+
+
+
+
+
+
+
+
+
